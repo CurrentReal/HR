5a3ce6d97c108cbe64f6e66a820e00450bc0157a
CurrentReal <lhj672400@naver.com>
2018-03-11 23:13:38 +0900

    add comment

--- HR/Algorithms/Implementation/Circular Array Rotation.cpp
+++ HR/Algorithms/Implementation/Circular Array Rotation.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+/*
+Because when the number of rotations(k) equals the size of the array(n), the array returns to it's original setup. For example, after 3 rotations, this array is back to original:
+[1,2,3] 0 original
+[3,1,2] 1 rotation
+[2,3,1] 2 rotations
+[1,2,3] 3 rotations //Back to original setup
+[3,1,2] 4 rotations // same as 1 rotation
+Therefore if you have 4 rotations, then 4 % 3 is 1, so you will get the same result as if you only had 1 rotation to begin with.
+*/
 int main(){
     int n, k, q;
     cin >> n >> k >> q;
