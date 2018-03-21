e05ee89e02c3bf23b86da5439cda7e09c756e74e
CurrentReal <lhj672400@naver.com>
2018-03-21 22:42:56 +0900

    add Sherlock and Squares

---
+++ HR/Algorithms/Implementation/Sherlock and Squares.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i <t; i++) {
+        int A, B;
+        cin >> A >> B;
+        int cnt = 0;
+        for(int j = A; j <= B; j++) {
+            int squaresInt = sqrt(j);
+            bool isSquaresInt = (squaresInt * squaresInt) == j;
+
+            if(isSquaresInt) {
+                cnt++;
+                j += squaresInt * 2;
+                // [1] 2 3 [4] 5 6 7 8 [9] 10 11 12 13 14 15 [16] 17 18 19 20 21 22 23 24 [25]
+                //     <2>    <   4   >    <       6        >     <           8          >
+                //     1*2       2*2              3*2                        4*2
+            }
+        }
+        cout << cnt << endl;
+
+        /* Editoiral
+        int sqCnt = floor(sqrt(B)) - ceil(sqrt(A));
+        cout << sqCnt + 1 << endl;
+        */
+    }
+
+    return 0;
+}
