575ae502b357c92b321178ca70ba3feb735debff
CurrentReal <lhj672400@naver.com>
2018-04-12 10:18:06 +0900

    add Super Reduced String

---
+++ HR/Algorithms/Implementation/Super Reduced String.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int i = 0;
+    while(s.size() != i)
+    {
+        if(s[i] == s[i+1])
+        {
+            s.erase(i, 2);
+            if(s.empty())
+            {
+                cout << "Empty String" << endl;
+                return 0;
+            }
+            i = 0;
+        }
+        else
+            i++;
+    }
+    cout << s << endl;
+    return 0;
+}
